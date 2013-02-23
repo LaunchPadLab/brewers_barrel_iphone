7a4be831d59df1115526906785b3942176bb9120
bhennes2 <bhennes2@gmail.com>
2013-02-23 16:04:14 -0600

    Initial commit - iPhone w00t

---
+++ www/js/template.js
@@ -0,0 +1,4 @@
+function show_template(recipe){
+	var html = '<ul class="list inset"><li>Name:<span class="recipe_style">'+recipe.name+'</span></li><li>Style:<span class="recipe_style">'+recipe.style+'</span></li></ul>';
+	return html
+}
